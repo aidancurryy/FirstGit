841dcc75df1847d66b771a480522866fe02ad406
Aidan Curry <aidancurryy@gmail.com>
2020-10-07 10:23:47 -0400

    Initial Commit

---
+++ FirstGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstGit
+//
+//  Created by  on 10/7/20.
+//  Copyright © 2020 ChemsApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
